9e20a0181ff101bfbc67f54d380149f612a60fcb
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-05-15 12:05:45 +0530

    Advanced notes app started

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(expressLayouts);
+
+app.set("layout", "layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/",require("./server/routes/index.js"))
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
